0463eaa9e6ab11c760b3986d2aa206b286123d9f
Greatemmet <greatemmet@gmail.com>
2023-09-17 08:34:42 +0100

    Retrieved and displayed name and image file input in the profile frame using Javascript

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-console.log("hello world")
+const generateBtn = document.querySelector(".generateBtn");
+
+const firstname = document.querySelector(".firstname");
+const lastname = document.querySelector(".lastname");
+const personImg = document.querySelector(".imgUpload");
+
+const imgDisplay = document.querySelector(".profilePic");
+const personFullName = document.querySelector(".personFullName");
+
+generateBtn.addEventListener("click", () => {
+  const file = personImg.files[0];
+  if (!firstname.value || !lastname.value) {
+    alert("Please Enter Name fields");
+  } else {
+    personFullName.innerHTML = `${firstname.value.toUpperCase()} ${lastname.value.toUpperCase()}`;
+
+    if (file) {
+      imgDisplay.src = `${URL.createObjectURL(file)}`;
+    } else {
+      alert("Upload an Image");
+    }
+  }
+});
